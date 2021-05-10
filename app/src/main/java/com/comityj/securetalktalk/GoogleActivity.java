72db5e08e6f42ad6f5a1685ab84d15b3344d1262
comityj <comit.yujin@gmail.com>
2021-05-11 00:07:38 +0900

    210511

--- app/src/main/java/com/comityj/securetalktalk/GoogleActivity.java
+++ app/src/main/java/com/comityj/securetalktalk/GoogleActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,33 +13,55 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class GoogleActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth ;
-    Button btnlogout, btnrevoke;
+    Button btnlogout, btnrevoke, btmchat;
     TextView tvname, tvemail;
-    CircleImageView imgprofile;
+    //CircleImageView imgprofile;
+    ImageView imgprofile;
+    private SignupActivity signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.google);
 
+        final Intent intent_L = new Intent(getApplicationContext(), LoginActivity.class);
+        final Intent intent_C = new Intent(getApplicationContext(), ChatActivity.class);
         mAuth = FirebaseAuth.getInstance();
 
+        btmchat = (Button) findViewById(R.id.chatbtn);
         btnlogout = (Button) findViewById(R.id.logout);
         btnrevoke = (Button) findViewById(R.id.revoke);
         tvname = (TextView) findViewById(R.id.profile_name);
         tvemail = (TextView) findViewById(R.id.profile_email);
-        imgprofile = (CircleImageView) findViewById(R.id.profile_img);
+        //imgprofile = (CircleImageView) findViewById(R.id.profile_img);
+        imgprofile = (ImageView) findViewById(R.id.profile_img);
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+//        signup = (SignupActivity)getApplicationContext();
+//        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+//        tvname = task.getResult().getUser().getDisplayName();
+
+        tvname.setText(user.getDisplayName());
+        imgprofile.setImageURI(user.getPhotoUrl());
+
+        btmchat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { startActivity(intent_C); }
+        });
 
         btnlogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 signOut();
+                startActivity(intent_L);
             }
         });
 
@@ -46,15 +69,9 @@ public class GoogleActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 revokeAccess();
+                startActivity(intent_L);
             }
         });
-
-
-
-        //Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-
-        //tvname = task.getResult().getUser().getDisplayName();
-
     }
 
     private void signOut() {
